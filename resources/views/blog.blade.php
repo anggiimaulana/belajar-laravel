fd6990bd580a9b893327fe1b3bf1f98087bd1335
anggii <anggabos120@gmail.com>
2024-09-05 15:29:08 +0700

    commit ke 2

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -11,5 +11,13 @@
     <a href="/about">About</a>
     <a href="/contact">Contact</a>
     <h2>Halaman Blog</h2>
+    <article>
+        <h3>Judul 1</h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi tenetur earum explicabo obcaecati reiciendis sit dolores, animi enim doloribus molestiae ipsam omnis et est voluptatum. Iure dolores distinctio neque quibusdam eius accusantium, itaque tempora dolor molestiae quo sequi nostrum consequatur eaque quisquam et nulla voluptatibus eligendi. Reprehenderit mollitia labore repellat.
+    </article>
+    <article>
+        <h3>Judul 2</h3>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia sapiente porro corporis quae quod sunt asperiores vitae voluptatibus amet, a repellat id consequuntur eum ratione saepe iusto inventore totam, alias impedit, voluptas neque consequatur magni aliquam ad. Impedit ex sapiente soluta commodi, fugiat molestias non?
+    </article>
 </body>
 </html>
